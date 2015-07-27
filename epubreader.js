41a054c6f5999ea63ba5d34a276869d05ed20a37
uggrock <ugg.rock@gmail.com>
2015-07-27 22:39:45 +0200

    js styling

--- epubreader.js
+++ epubreader.js
@@ -3,120 +3,115 @@
  * can be found in the LICENSE file. */
 
 define(function(require, exports, module) { 
-"use strict";
+  "use strict";
 
-    var ePub = require("ext/viewerEPUB/epubjs/epub.min");
-    var book, rendered;
-    var data;
-    var bookFileName;
+  var ePub = require("ext/viewerEPUB/epubjs/epub.min");
+  var book, rendered;
+  var data;
+  var bookFileName;
+  var defaultBookStyle = {
+    "font-size": "1.2em",
+    "text-align": "justify"
+  };
 
-    var defaultBookStyle = {
-        "font-size": "1.2em",
-        "text-align": "justify"
+  var options = {
+    bookPath : null,
+    version: 1, // Changing will cause stored Book information to be reloaded
+    restore: true,// Skips parsing epub contents, loading from localstorage instead
+    storage: false, // true (auto) or false (none) | override: 'ram', 'websqldatabase', 'indexeddb', 'filesystem'
+    spreads: false, // Displays two columns
+    fixedLayout : true, //-- Will turn off pagination
+    styles : {}, // Styles to be applied to epub
+    width : false,
+    height: false,
+  };
+
+  function getMimeType(file) {
+    var mimeTypes = {
+      "xhtml" : "application/xhtml+xml",
+      "jpg" : "image/jpeg",
+      "png": "image/png",
+      "htm":  "text/html",
+      "html": "text/html",
+      "htmls": "text/html"
     };
+    var ext = file.split(".").pop();
+    return mimeTypes[ext];
+  }
 
-    var options = {
-        bookPath : null,
-        version: 1, // Changing will cause stored Book information to be reloaded
-        restore: true,// Skips parsing epub contents, loading from localstorage instead
-        storage: false, // true (auto) or false (none) | override: 'ram', 'websqldatabase', 'indexeddb', 'filesystem'
-        spreads: false, // Displays two columns
-        fixedLayout : true, //-- Will turn off pagination
-        styles : {}, // Styles to be applied to epub
-        width : false,
-        height: false, 
-    };
+  EPUBJS.Unarchiver.prototype.loadLib = function() {
+    console.log("PUBJS.Unarchiver.prototype.loadLib - handle error ");
+  };
 
-    function getMimeType(file) {
-        var mimeTypes = {
-            "xhtml" : "application/xhtml+xml",
-            "jpg" : "image/jpeg",
-            "png": "image/png",
-            "htm":  "text/html",
-            "html": "text/html",
-            "htmls": "text/html"
-        }
-        var ext = file.split(".").pop();
-        return mimeTypes[ext];
+  EPUBJS.Unarchiver.prototype.openZip = function(zipUrl, callback) {
+    var unarchiver = this;
+    return unarchiver.getZipStorage(zipUrl).then(function(data) {
+      unarchiver.zip = data;
+      unarchiver.zip.getMimeType = getMimeType;
+    });
+  };
+
+  function loadBook(filePath, renderID) {
+    bookFileName = filePath;
+    book = ePub(filePath, options);
+    setStyle(defaultBookStyle);
+    rendered = book.renderTo(renderID);
+
+    book.on('renderer:locationChanged', function(locationCfi) {
+      storeLastPage(locationCfi);
+    });
+
+    book.on('book:pageChanged', function(location) {
+      console.log("pageChanged", location);
+    });
+
+    book.on('book:ready', function() {
+      if (options.restore) {
+        restoreLastPage();
+      }
+    });
+  }
+
+  function restoreLastPage() {
+    var locationCfi = storeLastPage();
+    book.displayChapter(locationCfi);
+  }
+
+  function storeLastPage(storeData) {
+    if (storeData !== null) {
+      localStorage.setItem(bookFileName, storeData);
+    } else {
+      return localStorage.getItem(bookFileName);
     }
+  }
 
-    EPUBJS.Unarchiver.prototype.loadLib = function () {
-        console.log("PUBJS.Unarchiver.prototype.loadLib - handle error ");
+  function setZipStoragePromise (fnZipPromise) {
+    EPUBJS.Unarchiver.prototype.getZipStorage = fnZipPromise;
+  }
+
+  function setZipData(zipData) {
+    data = zipData;
+  }
+
+  function prevPage() {
+    book.prevPage();
+  }
+
+  function nextPage() {
+    book.nextPage();
+  }
+
+  function setStyle (obj) {
+    for (var key in obj) {
+      book.setStyle(key, obj[key]);
     }
+  }
 
-    EPUBJS.Unarchiver.prototype.openZip = function(zipUrl, callback) {
-
-        var unarchiver = this;
-        return unarchiver.getZipStorage(zipUrl).then(function(data) {
-            unarchiver.zip = data;
-            unarchiver.zip.getMimeType = getMimeType;
-        });
-    };
-
-    function loadBook(filePath, renderID) {
-        
-        bookFileName = filePath;
-
-        book = ePub(filePath, options);
-        setStyle(defaultBookStyle);
-        rendered = book.renderTo(renderID);
-
-        book.on('renderer:locationChanged', function(locationCfi) {
-            storeLastPage(locationCfi);
-        });
-
-        book.on('book:pageChanged', function(location) {
-            console.log("pageChanged", location);
-        });
-
-        book.on('book:ready', function() {
-            if(options.restore) {
-                restoreLastPage();
-            }
-        });
-    }
-
-    function restoreLastPage() {
-        var locationCfi = storeLastPage();
-        book.displayChapter(locationCfi);
-    }
-
-    function storeLastPage(storeData) {
-        
-        if(storeData != null) {
-            localStorage.setItem(bookFileName, storeData);
-        } else {
-            return localStorage.getItem(bookFileName);
-        }
-    }
-
-    function setZipStoragePromise (fnZipPromise) {
-        EPUBJS.Unarchiver.prototype.getZipStorage = fnZipPromise;
-    }
-
-    function setZipData(zipData) {
-        data = zipData;
-    } 
-
-    function prevPage() {
-        book.prevPage();
-    }
-
-    function nextPage() {
-        book.nextPage();
-    }
-
-    function setStyle (obj) {
-        for (var key in obj) {
-            book.setStyle(key, obj[key]);
-        }
-    }
-
-    exports.options = options;
-    exports.loadBook = loadBook;
-    exports.prevPage = prevPage;
-    exports.nextPage = nextPage;
-    exports.setZipStoragePromise = setZipStoragePromise;
-    exports.setZipData = setZipData;
-    exports.setStyle = setStyle;
+  exports.options = options;
+  exports.loadBook = loadBook;
+  exports.prevPage = prevPage;
+  exports.nextPage = nextPage;
+  exports.setZipStoragePromise = setZipStoragePromise;
+  exports.setZipData = setZipData;
+  exports.setStyle = setStyle;
 });
--- extension.js
+++ extension.js
@@ -3,160 +3,155 @@
  * can be found in the LICENSE file. */
 
 define(function(require, exports, module) {
-"use strict";
+  "use strict";
 
-    console.log("Loading viewerEPUB");
+  console.log("Loading viewerEPUB");
 
-    exports.id = "viewerEPUB"; // ID should be equal to the directory name where the ext. is located
-    exports.title = "EPUB Viewer";
-    exports.type = "viewer";
-    exports.supportedFileTypes = [ "epub" ];
-    
-    var JSZip = require("jszip");
-    var TSCORE = require("tscore");
-    var reader = require("ext/viewerEPUB/epubreader");
+  exports.id = "viewerEPUB"; // ID should be equal to the directory name where the ext. is located
+  exports.title = "EPUB Viewer";
+  exports.type = "viewer";
+  exports.supportedFileTypes = ["epub"];
 
-    var extensionDirectory = TSCORE.Config.getExtensionPath()+"/"+exports.id;
-    
-    function getZipStorage(zipUrl) {
+  var JSZip = require("jszip");
+  var TSCORE = require("tscore");
+  var reader = require("ext/viewerEPUB/epubreader");
 
-        var promise = new Promise( function (resolve, reject) {
-            TSCORE.IO.getFileContent(zipUrl, function(jsArrayBuffer) {
-                var zipFile = new JSZip(jsArrayBuffer);
-                resolve(zipFile);
-            }, function(error) {
-                console.log("getZipStorage error: " + error);
-                reject(error);
-            });
-        });
+  var extensionDirectory = TSCORE.Config.getExtensionPath() + "/" + exports.id;
 
-        return promise;
+  function getZipStorage(zipUrl) {
+    var promise = new Promise(function(resolve, reject) {
+      TSCORE.IO.getFileContent(zipUrl, function(jsArrayBuffer) {
+        var zipFile = new JSZip(jsArrayBuffer);
+        resolve(zipFile);
+      }, function(error) {
+        console.log("getZipStorage error: " + error);
+        reject(error);
+      });
+    });
+
+    return promise;
+  }
+
+  function initViewerUI(elementID, renderID) {
+    var styleArrow =  {
+      "position": "absolute",
+      "top": "50%",
+      "margin-top": "-32px",
+      "font-size": "64px",
+      "color": "#D8D8D8",
+      "font-family": "arial, sans-serif",
+      "font-weight": "bold",
+      "cursor": "pointer",
+      "-webkit-user-select": "none",
+      "-moz-user-select": "none",
+      "user-select": "none"
+    };
+
+    var styleMain = {
+      "position": "absolute",
+      "width": "100%",
+      "height": "100%"
+    };
+
+    var styleArea = {
+      "width": "80%",
+      "height": "80%",
+      "margin": "5% auto",
+    };
+
+    var $prev = $("<div>‹</div>").attr('id', 'prev').css({"left": "40px"}).css(styleArrow).click(reader.prevPage);
+    var $next = $("<div>›</div>").attr('id', 'next').css({"right": "40px"}).css(styleArrow).click(reader.nextPage);
+
+    var $area = $("<div/>").attr('id', renderID).css(styleArea);
+    var $main = $("<div/>").attr('id', 'main').css(styleMain).append($prev).append($area).append($next);
+
+    $('#' + elementID).append($main);
+  }
+
+  exports.init = function(filePath, elementID) {
+    console.log("Initalization EPUB Viewer...");
+    var renderID = getRandomID("epub");
+    initViewerUI(elementID, renderID);
+    if (isCordova !== true) {
+      reader.setZipStoragePromise(getZipStorage);
+      reader.loadBook(filePath, renderID);
+    } else {
+      loadEpubFile(filePath, function(extractedPath) {
+        reader.loadBook(extractedPath, renderID);
+      });
     }
+  };
 
-    function initViewerUI(elementID, renderID) {
-        var styleArrow =  {
-            "position": "absolute",
-            "top": "50%",
-            "margin-top": "-32px",
-            "font-size": "64px",
-            "color": "#D8D8D8",
-            "font-family": "arial, sans-serif",
-            "font-weight": "bold",
-            "cursor": "pointer",
-            "-webkit-user-select": "none",
-            "-moz-user-select": "none",
-            "user-select": "none"
-        };
+  exports.viewerMode = function() {
+    console.log("viewerMode not supported on this extension");
+  };
 
-        var styleMain = {
-            "position": "absolute",
-            "width": "100%",
-            "height": "100%"
-        };
+  exports.setContent = function() {
+    console.log("setContent not supported on this extension");
+  };
 
-        var styleArea = {
-            "width": "80%",
-            "height": "80%",
-            "margin": "5% auto",
-        };
+  exports.getContent = function() {
+    console.log("getContent not supported on this extension");
+  };
 
-        var $prev = $("<div>‹</div>").attr('id','prev').css({"left": "40px"}).css(styleArrow).click(reader.prevPage);
-        var $next = $("<div>›</div>").attr('id','next').css({"right": "40px"}).css(styleArrow).click(reader.nextPage);
-        
-        var $area = $("<div/>").attr('id', renderID).css(styleArea); 
-        var $main = $("<div/>").attr('id','main').css(styleMain)
-            .append($prev).append($area).append($next);
+  function baseName(path) {
+    return path.split("/").pop();
+  }
+  function dirName(path) {
+    var res = path.substring(0, path.lastIndexOf("/"));
+    return res.length > 0 ? res : undefined;
+  }
 
-        $('#'+elementID).append($main); 
+  function loadEpubFile(filePath, resultCallback) {
+    var extrfolder = isCordovaiOS ? cordova.file.dataDirectory : cordova.file.externalDataDirectory;
+    var pathToImport = extrfolder + "/" + baseName(filePath) + ".IMP/";
 
-    }
+    TSCORE.IO.getFileContent(filePath, function(jsArrayBuffer) {
+      var zipFile = new JSZip(jsArrayBuffer);
+      var lastDir, currentDir;
+      TSCORE.IO.createDirectory(pathToImport, true);
 
-    exports.init = function(filePath, elementID) {
-        console.log("Initalization EPUB Viewer...");
-        var renderID = getRandomID("epub");
-        initViewerUI(elementID, renderID);
-        if(isCordova !== true) {
-            reader.setZipStoragePromise(getZipStorage);
-            reader.loadBook(filePath, renderID);   
+      for (var fileName in zipFile.files) {
+        currentDir = dirName(fileName);
+        if (lastDir !== currentDir) {
+          TSCORE.IO.createDirectory(pathToImport + currentDir, true);
+        }
+
+        if (JSZip.support.arraybuffer) {
+          var buffer = zipFile.file(fileName).asArrayBuffer();
+          var extractedFilePath = pathToImport;
+          if (currentDir) {
+            extractedFilePath += currentDir + "/";
+          }
+          extractedFilePath += baseName(fileName);
+
+          TSCORE.IO.saveBinaryFile(extractedFilePath, buffer, true, true);
         } else {
-            loadEpubFile(filePath, function(extractedPath) {
-                reader.loadBook(extractedPath, renderID);
-            });
+          TSCORE.showAlertDialog("JSZip dont support arraybuffer exit !");
+          return;
         }
-    };
-    
-    exports.viewerMode = function() {
-        console.log("viewerMode not supported on this extension");  
-    };
-    
-    exports.setContent = function() {
-        console.log("setContent not supported on this extension");  
-    };
-    
-    exports.getContent = function() {
-        console.log("getContent not supported on this extension");  
-    };
 
-    function baseName(path) {
-        return path.split("/").pop();
+        lastDir = currentDir;
+      }
+
+      setTimeout(function() {
+        pathToImport = pathToImport.replace("file://", "");
+        resultCallback(pathToImport, zipFile);
+      }, 1000);
+
+    }, function(error) {
+      TSCORE.showAlertDialog(error);
+    });
+  }
+
+  function getRandomID(prefix, length) {
+    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+    var string_length = length || 8;
+    var randomstring = '';
+    for (var i = 0; i < string_length; i++) {
+      var rnum = Math.floor(Math.random() * chars.length);
+      randomstring += chars.substring(rnum, rnum + 1);
     }
-    function dirName(path) {
-        var res = path.substring(0, path.lastIndexOf("/"));
-        return res.length > 0 ? res : undefined;
-    }
-
-    function loadEpubFile(filePath, resultCallback) {
-
-        var extrfolder = isCordovaiOS ? cordova.file.dataDirectory : cordova.file.externalDataDirectory;
-        var pathToImport = extrfolder + "/" + baseName(filePath) +".IMP/";
-        
-        TSCORE.IO.getFileContent(filePath, function(jsArrayBuffer) {
-            var zipFile = new JSZip(jsArrayBuffer);
-            var lastDir, currentDir;
-            TSCORE.IO.createDirectory(pathToImport, true);
-
-            for (var fileName in zipFile.files) {
-                
-                currentDir = dirName(fileName);
-                if (lastDir !== currentDir) {
-                    TSCORE.IO.createDirectory(pathToImport + currentDir, true);
-                } 
-
-                if (JSZip.support.arraybuffer) {
-                    var buffer = zipFile.file(fileName).asArrayBuffer(); 
-                    var extractedFilePath = pathToImport;
-                    if(currentDir) {
-                        extractedFilePath += currentDir + "/";
-                    }
-                    extractedFilePath += baseName(fileName);
-
-                    TSCORE.IO.saveBinaryFile(extractedFilePath, buffer, true, true);
-                } else {
-                    TSCORE.showAlertDialog("JSZip dont support arraybuffer exit !");
-                    return;
-                }
-
-                lastDir = currentDir;
-           }
-
-        setTimeout( function() {
-            pathToImport = pathToImport.replace("file://", "");
-            resultCallback(pathToImport, zipFile); 
-        }, 1000);
-   
-        }, function(error) {
-            TSCORE.showAlertDialog(error);
-        });
-    }
-
-    function getRandomID(prefix, length) {
-        var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
-        var string_length = length || 8;
-        var randomstring = '';
-        for (var i=0; i < string_length; i++) {
-            var rnum = Math.floor(Math.random() * chars.length);
-            randomstring += chars.substring(rnum, rnum + 1);
-        }
-        return prefix ? prefix + "-" + randomstring : randomstring;
-    }
+    return prefix ? prefix + "-" + randomstring : randomstring;
+  }
 });
